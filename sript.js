6444aa2eb3787b8a54f8bcd1a8ad9be668dfbf1f
joaoEmanuel6 <joaoemanuel689@gmail.com>
2022-01-19 09:30:48 -0400

    Desafio 03

---
+++ sript.js
@@ -0,0 +1,24 @@
+// De 1 a 10 com for
+for (let i=1 ; i<=10; i ++) {
+    console.log(i)
+}
+
+// De 1 a 10 com while
+let number = 1 
+while (number<=10) {
+    console.log(number)
+    number ++
+}
+
+// números ímpares de 1 a 100
+let a = 1 
+while (a <=100) {
+    console.log(a)
+    a = a + 2
+}
+// números pares de 0 a 100
+let b = 0
+while (b <=100) {
+    console.log(b)
+    b = b + 2 
+}
